35230dc9ac78ad3a64a8452381661f8bd6bcc48d
Chueshova Olga <chueshova_olga@mail.ru>
2022-08-16 03:40:01 +0300

    slider reviews

--- js/components/categories.js
+++ js/components/categories.js
@@ -1,81 +1,43 @@
-const newP = document.querySelector('.categories__list--new');
-const newCatalog = document.querySelector('.categories__catalog--new-product');
 
-function displayNew() {
-    newCatalog.classList.add('categories__catalog--new-product-active');
-    newP.classList.add('categories__list--link-active');
-    hitCatalog.classList.add('categories__catalog--hit-product-noactive');
-    hit.classList.add('categories__list--link-noactive');
-    popularCatalog.classList.add('categories__catalog--popular-product-noactive');
-    popular.classList.add('categories__list--link-noactive');
-    stockCatalog.classList.add('categories__catalog--stock-product-noactive');
-    stock.classList.add('categories__list--link-noactive');
-}
-
-newP.addEventListener('click', displayNew);
-
-
-
-const hit = document.querySelector('.categories__list--hit');
-const hitCatalog = document.querySelector('.categories__catalog--hit-product');
-
-function displayHit() {
-    hitCatalog.classList.add('categories__catalog--new-product-active');
-    hit.classList.add('categories__list--link-active');
-    newCatalog.classList.add('categories__catalog--new-product-noactive');
-    newP.classList.add('categories__list--link-noactive');
-    popularCatalog.classList.add('categories__catalog--popular-product-noactive');
-    popular.classList.add('categories__list--link-noactive');
-    stockCatalog.classList.add('categories__catalog--stock-product-noactive');
-    stock.classList.add('categories__list--link-noactive');
-}
-
-hit.addEventListener('click', displayHit);
-
-
-
-const popular = document.querySelector('.categories__list--popular');
-const popularCatalog = document.querySelector('.categories__catalog--popular-product');
-
-function displayPopular() {
-    popularCatalog.classList.add('categories__catalog--popular-product-active');
-    popular.classList.add('categories__list--link-active');
-    newCatalog.classList.add('categories__catalog--new-product-noactive');
-    newP.classList.add('categories__list--link-noactive');
-    hitCatalog.classList.add('categories__catalog--hit-product-noactive');
-    hit.classList.add('categories__list--link-noactive');
-    stockCatalog.classList.add('categories__catalog--stock-product-noactive');
-    stock.classList.add('categories__list--link-noactive');
-}
-
-popular.addEventListener('click', displayPopular);
-
-
-
-const stock = document.querySelector('.categories__list--stock');
-const stockCatalog = document.querySelector('.categories__catalog--stock-product');
-
-function displayStock() {
-    stockCatalog.classList.add('categories__catalog--stock-product-active');
-    stock.classList.add('categories__list--link-active');
-    newCatalog.classList.add('categories__catalog--new-product-noactive');
-    newP.classList.add('categories__list--link-noactive');
-    hitCatalog.classList.add('categories__catalog--hit-product-noactive');
-    hit.classList.add('categories__list--link-noactive');
-    popularCatalog.classList.add('categories__catalog--popular-product-noactive');
-    popular.classList.add('categories__list--link-noactive');
-}
-
-stock.addEventListener('click', displayStock);
-
-const catalog = document.querySelectorAll('.categories__list--link');
-
-function displayNone(target, callback) {
+function callback(target, callback) {
     function handleEvent(evt) {
         if (!target.contains(evt.target)) {
             callback();
         }
     }
+    document.addEventListener('click', handleEvent);
 }
 
-catalog.addEventListener('click', handleEvent)
+
+const nav = document.querySelector('.categories');
+const linkCollection = document.querySelectorAll('.categories__list--item');
+const catalogCollection = document.querySelectorAll('.categories__catalog--item');
+
+function addCatalog(evt) {
+    const target = evt.target.closest('.categories__list--item');
+    if (target) {
+        target.classList.add('categories__list--item-active');
+        const isActive = target.classList.contains('categories__list--item-active');
+        function showCatalog {
+            target.querySelector('.categories__catalog--item'),
+                opacity: isActive ? 1 : 0,
+                    visibility: isActive ? visible : hidden,
+
+                        useClickOtherLink(target, function () {
+                            target.classList.remove('categories__list--item-active');
+                            opacity: isActive ? 0 : 1,
+                            visibility: isActive ? hidden : visible,
+                        });
+        }
+
+    }
+
+
+}
+
+
+
+
+
+nav.addEventListener('click', addCatalog);
+
--- js/components/slider-reviews.js
+++ js/components/slider-reviews.js
@@ -1,20 +1,23 @@
 const swiper = new Swiper('.swiperReviews', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 20,
-    watchSlidesProgress: true,
-    
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  slideActiveClass: 'swiper-slide-active',
+  slideDublicateActiveClass: 'swiper-slide-duplicate-active',
+  // watchOverflow: true,
+  watchSlidesProgress: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
