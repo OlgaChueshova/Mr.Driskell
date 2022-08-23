241a905361f1fe2a2fd78c30fc30f45b034190f6
John Doe <johndoe@example.com>
2022-08-23 19:30:47 +0300

    gfjh,lkj;.

--- js/components/product-card.js
+++ js/components/product-card.js
@@ -13,10 +13,6 @@ function like(evt) {
     target.classList.toggle('product-card__like--active');
 }
 
-productCard.addEventListener('click', like);
-
-
-
 
 function addInBasket(evt) {
     let target = evt.target.closest('.product-card__inbasket');
@@ -31,124 +27,46 @@ function addInBasket(evt) {
     target.classList.add('product-card__inbasket--active');
 }
 
-productCard.addEventListener('click', addInBasket);
-
-
-
-
 
 function selectProducts(evt) {
     evt.preventDefault()
 
-    const inputCollection = document.querySelectorAll('.product-card__input');
-    const btnPlusCollection = document.querySelectorAll('.btn--plus');
-    const btnMinusColection = document.querySelectorAll('.btn--minus');
-    const formCollection = document.querySelectorAll('.product-card__form');
-
-
-    let target = evt.target.closest('.product-card__form');
-
-
-    for (let i = 0; i < formCollection.length; i++) {
-
-        const form = formCollection[i];
-
-        const inputCollection = document.querySelectorAll('.product-card__input');
-
-        for (let i = 0; i < inputCollection.length; i++) {
-            const current = inputCollection[i];
-            const value = Number(current.value);
+    const target = evt.target.closest('.product-card__form');
+    console.log(evt.target)
+    if (target) {
+        const input = target.querySelector('.product-card__input');
+        const value = Number(input.value);
+        if (evt.target.closest('.btn--plus')) {
+            input.value = value + 1;
         }
- 
-
-    form.addEventListener('click', function (evt) {
-
-        let target = evt.target.closest('.btn--plus', '.btn--minus')
-
-
-        if (evt.target.classList('.btn--plus')) {
-            for (let i = 0; i < btnPlusCollection.length; i++) {
-                const btnPlus = btnPlusCollection[i];
+        if (evt.target.closest('.btn--minus')) {
+            if (value > 1) {
+                input.value = value - 1;
             }
+        
+            if (value === 1) {
+                    
+                target.querySelector('btn--minus')
+                
+                
+            }  
+                
+            
 
-            btnPlus.addEventListener('click', function (evt) {
-                input.value = value + 1;
-            })
         }
-        if (evt.target.classList('.btn--minus')) {
-            for (let i = 0; i < btnMinusCollection.length; i++) {
-                const btnMinus = btnMinusCollection[i];
-            }
-            btnMinus.addEventListener('click', function (evt) {
-                if (value > 1) {
-                    input.value = value - 1;
-                }
-                if (value = 1) {
-                    collback(addInBasket);
-                }
 
-            })
-        }
-    })
-}
+    }
 
 }
 
 
 
-//     const btnPlus = evt.target.closest('.btn--plus');
-//     const btnMinus = evt.target.closest('.btn--minus');
-//     const card = document.querySelector('.product-card__form');
-//     const input = card.querySelector('.product-card__input');
-//     const value = Number(input.value);
 
-
-//     if (btnPlus) {
-//         input.value = value + 1;
-//     }
-//     if (btnMinus) {
-//         if (value > 1) {
-//             input.value = value - 1;
-//         }
-//     }
-// }
-// )
-
-
-//     const btnPlusCollection = document.querySelectorAll('.btn--plus');
-//     const btnMinusColection = document.querySelectorAll('.btn--minus');
-// let target = evt.target.closest('.btn--plus, .btn--minus')
-
-
-//     if (evt.target.closest('.btn--plus')) {
-//         for (let i = 0; i < btnPlusCollection.length; i++) {
-//             const btnPlus = btnPlusCollection[i];
-//         }
-
-// // как найти именно тот input, который нужен ?
-
-//             btnPlus.addEventListener('click', function (evt) {
-//                 input.value = value + 1;
-//             })
-//     }
-//     if (evt.target.closest('.btn--minus')) {
-//         for (let i = 0; i < btnMinusCollection.length; i++) {
-//             const btnMinus = btnMinusCollection[i];
-//         }
-//         btnMinus.addEventListener('click', function (evt) {
-//             if (value > 1) {
-//                 input.value = value - 1;
-//             }
-//             if (value = 1) {
-//                 collback(addInBasket);
-//             }
-
-//         })
-//     }
-
-
-
-productCard.addEventListener('click', selectProducts);
+productCard.addEventListener('click', function (evt) {
+    selectProducts(evt);
+    addInBasket(evt);
+    like(evt);
+});
 
 
 
